0f06160b488da55becece117e38823cb7f58ed8a
deathsovushka <vlad_leonov_09@mail.ru>
2023-05-05 16:55:47 +0300

    1

--- src/main/java/org/mythril/slskins/Command.java
+++ src/main/java/org/mythril/slskins/Command.java
@@ -1,2 +1,106 @@
-package org.mythril.slskins;public class Command {
+package org.mythril.slskins;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.skinsrestorer.api.SkinsRestorerAPI;
+import net.skinsrestorer.api.property.IProperty;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Command implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, org.bukkit.command.@NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        if(commandSender instanceof Player){
+            if(!(commandSender.hasPermission("skiner.use"))) return true;
+            Player p = (Player) commandSender;
+            SkinsRestorerAPI api = SLSkins.getAPI();
+            if(strings.length == 0){
+                p.sendMessage("/slskins add <Название> - Загрузка текущего скина в библиотеку");
+                p.sendMessage("/slskins remove <Название> - Удаление скина с этим названием из библиотеки");
+                p.sendMessage("/slskins menu - Библиотека ваших скинов");
+                return true;
+            }
+            if(strings[0].equals("add")){
+                if(!(strings[1].isEmpty())){
+                    IProperty skin = api.getSkinData(api.getSkinName(p.getName()));
+                    if(skin != null){
+                        String url = api.getSkinTextureUrl(skin);
+                        if(!(SLSkins.skinlist.containsKey(p.getUniqueId() + "%#@#%" + strings[1]))){
+                            SLSkins.skinlist.put(p.getUniqueId() + "%#@#%" + strings[1], url);
+                            p.sendMessage("Скин добавлен!");
+                        } else {
+                            p.sendMessage("У вас уже есть скин с таким названием!");
+                        }
+                    }
+                } else {
+                    p.sendMessage("Используйте /slskins add <Название> для добавления вашего текущего скина");
+                }
+            } else if(strings[0].equals("menu")){
+                boolean container = false;
+                int index = 0;
+                Inventory inventory = Bukkit.createInventory(null, 54, "Библиотека скинов");
+                for (String string: SLSkins.skinlist.keySet()
+                     ) {
+                    if(string.contains(p.getUniqueId().toString())){
+                        if(index > 52){
+                            continue;
+                        }
+                        String[] strings1 = string.split("%#@#%");
+                        container = true;
+                        ItemStack itemstack = new ItemStack(Material.PLAYER_HEAD);
+                        ItemMeta ItemMeta = itemstack.getItemMeta();
+                        ItemMeta.displayName(Component.text(strings1[1], TextColor.color(255, 255, 255)).decoration(TextDecoration.ITALIC, false));
+                        List<Component> lore = new ArrayList<>();
+                        lore.add(Component.text(""));
+                        lore.add(Component.text("  Нажмите для активации этого скина!"));
+                        lore.add(Component.text(""));
+                        ItemMeta.lore(lore);
+                        ItemMeta.setCustomModelData(index);
+                        itemstack.setItemMeta(ItemMeta);
+                        inventory.setItem(index, itemstack);
+                        index++;
+                    }
+                }
+                if(!(container)){
+                    p.sendMessage("У вас нет скинов, используйте /slskins add <Название> для добавления вашего текущего скина");
+                } else {
+                    SLSkins.list.add(p);
+                    p.openInventory(inventory);
+                }
+            } else if(strings[0].equals("remove")){
+                if(strings.length < 2){
+                    p.sendMessage("Вы забыли указать название скина для удаления");
+                    return true;
+                }
+                if(SLSkins.skinlist.containsKey(p.getUniqueId() + "%#@#%" + strings[1])){
+                    SLSkins.skinlist.remove(p.getUniqueId() + "%#@#%" + strings[1]);
+                    p.sendMessage("Скин удалён!");
+                } else {
+                    p.sendMessage("У вас нет скина с таким названием!");
+                }
+            } else {
+                p.sendMessage("/slskins add <Название> - Загрузка текущего скина в библиотеку");
+                p.sendMessage("/slskins remove <Название> - Удаление скина с этим названием из библиотеки");
+                p.sendMessage("/slskins menu - Библиотека ваших скинов");
+                return true;
+            }
+        } else {
+            commandSender.sendMessage("Доступно только Спонсорам!");
+        }
+        return true;
+    }
 }
--- src/main/java/org/mythril/slskins/SLSkins.java
+++ src/main/java/org/mythril/slskins/SLSkins.java
@@ -1,17 +1,109 @@
 package org.mythril.slskins;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.skinsrestorer.api.PlayerWrapper;
+import net.skinsrestorer.api.SkinVariant;
+import net.skinsrestorer.api.SkinsRestorerAPI;
+import net.skinsrestorer.api.exception.SkinRequestException;
+import net.skinsrestorer.api.property.IProperty;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public final class SLSkins extends JavaPlugin {
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
+public final class SLSkins extends JavaPlugin implements @NotNull Listener {
+    public static SkinsRestorerAPI skinsRestorerAPI;
+
+    public static HashMap<String, String> skinlist = new HashMap<>();
+
+    public static List<Player> list = new ArrayList<>();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        loadHashMapFromFile();
+        Objects.requireNonNull(this.getCommand("slskins")).setExecutor(new Command());
+        Bukkit.getPluginManager().registerEvents(this, this);
+        skinsRestorerAPI = SkinsRestorerAPI.getApi();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        saveHashMapToFile();
+    }
+
+    public static SkinsRestorerAPI getAPI(){
+        return skinsRestorerAPI;
+    }
+
+    @EventHandler
+    public void onClick(InventoryClickEvent event) throws SkinRequestException {
+        if(list.contains((Player) event.getWhoClicked())){
+            Player player = (Player) event.getWhoClicked();
+            if(event.getCurrentItem() != null){
+                event.setCancelled(true);
+                ItemMeta itemMeta = event.getCurrentItem().getItemMeta();
+                for (String string: skinlist.keySet()
+                     ) {
+                    if(string.contains(player.getUniqueId().toString())){
+                        String[] strings = string.split("%#@#%");
+                        ItemStack is = new ItemStack(Material.PLAYER_HEAD);
+                        ItemMeta ItemMeta = is.getItemMeta();
+                        ItemMeta.displayName(Component.text(strings[1], TextColor.color(255, 255, 255)).decoration(TextDecoration.ITALIC, false));
+                        if(itemMeta.displayName().equals(ItemMeta.displayName())){
+                            IProperty iProperty = getAPI().genSkinUrl(skinlist.get(string), SkinVariant.CLASSIC);
+                            getAPI().applySkin(new PlayerWrapper(player), iProperty);
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "skin url " + event.getWhoClicked().getName() + " " + skinlist.get(string));
+                            event.getWhoClicked().closeInventory();
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public static void saveHashMapToFile() {
+        try {
+            FileOutputStream fos = new FileOutputStream("myHashMap.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(skinlist);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void loadHashMapFromFile() {
+        try {
+            FileInputStream fis = new FileInputStream("myHashMap.ser");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            skinlist = (HashMap<String, String>) ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @EventHandler
+    public void onClose(InventoryCloseEvent event){
+        list.remove((Player) event.getPlayer());
     }
 }
